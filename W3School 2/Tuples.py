10f275e577f2409cb90b9929428cff638670fa1d
Aiya <a_gabdulkyzy@kbtu.kz>
2025-09-25 23:11:53 +0600

    Tuples

--- W3School 2/Tuples.py
+++ W3School 2/Tuples.py
@@ -106,7 +106,7 @@ print(thistuple)
 #-------------------------------------------------------------------------------------------------
 thistuple = ("apple", "banana", "cherry")
 del thistuple
-print(thistuple) #this will raise an error because the tuple no longer exists
+#print(thistuple) #this will raise an error because the tuple no longer exists
 
 #-------------------------------------------------------------------------------------------------
 fruits = ("apple", "banana", "cherry")
