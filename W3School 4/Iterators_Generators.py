e494725371e27755c786e603592dfd2fdf371b95
Aiya <a_gabdulkyzy@kbtu.kz>
2025-10-09 22:39:31 +0600

    update

--- W3School 4/Iterators_Generators.py
+++ W3School 4/Iterators_Generators.py
@@ -1,54 +1,41 @@
 # 1. Generator that generates squares of numbers up to N--------------------------------------------------------------------------------
-def gen_squares(N):
-    for i in range(N+1):
-        yield i * i
-
-print("Squares up to 5:")
-for val in gen_squares(5):
-    print(val, end=" ")
-print("\n")
+def generate_squares(N):
+    for i in range(1, N + 1):
+        yield i ** 2  
+for square in generate_squares(5):
+    print(square)
 
 
 #Generator to print even numbers between 0 and n (comma separated)-----------------------------------------------------------------------------------------------------
-def gen_evens(n):
-    for i in range(n+1):
+def even_numbers(n):
+    for i in range(0, n + 1):
         if i % 2 == 0:
             yield i
-
-n = int(input("Enter a number for even numbers: "))
-print(",".join(str(num) for num in gen_evens(n)))
+n = int(input("Enter a number: "))
+print(", ".join(str(num) for num in even_numbers(n)))
 
 
 #Generator for numbers divisible by 3 and 4 between 0 and n------------------------------------------------------------------------------------
-def div_by_3_and_4(n):
-    for i in range(n+1):
+def divisible_by_3_and_4(n):
+    for i in range(0, n + 1):
         if i % 3 == 0 and i % 4 == 0:
             yield i
-
-print("Divisible by 3 and 4 up to 50:")
-for num in div_by_3_and_4(50):
-    print(num, end=" ")
-print("\n")
+n = int(input("Enter the limit: "))
+for num in divisible_by_3_and_4(n):
+    print(num)
 
 
 #Generator squares between a and b--------------------------------------------------------------------------------
 def squares(a, b):
-    for i in range(a, b+1):
-        yield i * i
-
-print("Squares between 3 and 7:")
-for val in squares(3, 7):
-    print(val, end=" ")
-print("\n")
-
+    for i in range(a, b + 1):
+        yield i ** 2
+for val in squares(2, 6):
+    print(val)
 
 #Generator countdown from n to 0----------------------------------------------------------------------------------
 def countdown(n):
     while n >= 0:
         yield n
         n -= 1
-
-print("Countdown from 5:")
-for val in countdown(5):
-    print(val, end=" ")
-print()
+for num in countdown(5):
+    print(num)
--- W3School 4/Math_library.py
+++ W3School 4/Math_library.py
@@ -1,22 +1,22 @@
 import math
 
 #Convert degree to radian-------------------------------------------------------------------------------------
-degree = 15
-radian = math.radians(degree)
-print("Degree:", degree, "-> Radian:", round(radian, 6))
+degree = float(input("Input degree: "))
+radian = degree * (math.pi / 180)
+print("Output radian:", round(radian, 6))
 
 #Area of a trapezoid--------------------------------------------------------------------------------------------------
 height = 5
 base1 = 5
 base2 = 6
-trapezoid_area = (base1 + base2) / 2 * height
+trapezoid_area = 0.5 * (base1 + base2) * height
 print("Area of trapezoid:", trapezoid_area)
 
 #Area of a regular polygon---------------------------------------------------------------------------------------
 n = 4   # number of sides
 s = 25  # length of a side
 polygon_area = (n * s**2) / (4 * math.tan(math.pi / n))
-print("Area of polygon:", polygon_area)
+print("Area of polygon:", round(polygon_area, 2))
 
 #Area of a parallelogram-------------------------------------------------------------------------
 base = 5
